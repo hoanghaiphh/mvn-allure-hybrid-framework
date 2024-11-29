33da55eaac7f25101a248f4bccbc4ef82a89c6d2
Hoang Hai <hoanghai.phh@gmail.com>
2024-11-29 19:38:20 +0700

    initial commit

---
+++ src/test/java/nopcommerce/user/Level_16_Allure_2.java
@@ -0,0 +1,91 @@
+package nopcommerce.user;
+
+import commons.BaseTest;
+import commons.GlobalConstants;
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pageObjects.nopcommerce.PageGenerator;
+import pageObjects.nopcommerce.user.HomePageObject;
+import pageObjects.nopcommerce.user.LoginPageObject;
+import pageObjects.nopcommerce.user.RegisterPageObject;
+import pageObjects.nopcommerce.user.myAccount.CustomerInfoPageObject;
+
+@Feature("User")
+public class Level_16_Allure_2 extends BaseTest {
+
+    private CustomerInfoPageObject customerInfoPage;
+    private HomePageObject homePage;
+    private RegisterPageObject registerPage;
+    private LoginPageObject loginPage;
+
+    private String firstName = "Automation";
+    private String lastName = "Testing";
+    private String dayOfBirth = "15";
+    private String monthOfBirth = "October";
+    private String yearOfBirth = "1989";
+    private String companyName = "Online 29";
+    private String emailAddress = firstName + lastName + getRandomNumber() + "@gmail.com";
+    private String password = "Abcd@1234";
+
+    @Parameters("browser")
+    @BeforeClass
+    public void beforeClass(String browserName) {
+        driver = openBrowserWithUrl(browserName, GlobalConstants.NOPCOMMERCE_LOCAL);
+        homePage = PageGenerator.getHomePage(driver);
+    }
+
+    @Description("User_01_Register")
+    @Severity(SeverityLevel.NORMAL)
+    @Test
+    public void User_01_Register() {
+        registerPage = (RegisterPageObject) homePage.clickOnHeaderLink("Register");
+        
+        registerPage.clickOnGenderMaleRadio();
+        registerPage.sendKeyToFirstnameTextbox(firstName);
+        registerPage.sendKeyToLastnameTextbox(lastName);
+//        registerPage.selectItemInDayDropdown(dayOfBirth);
+//        registerPage.selectItemInMonthDropdown(monthOfBirth);
+//        registerPage.selectItemInYearDropdown(yearOfBirth);
+        registerPage.sendKeyToEmailTextbox(emailAddress);
+        registerPage.sendKeyToCompanyTextbox(companyName);
+        registerPage.sendKeyToPasswordTextbox(password);
+        registerPage.sendKeyToConfirmPasswordTextbox(password);
+        registerPage.clickOnRegisterButton();
+        
+        verifyEquals(registerPage.getRegisterSuccessMessage(), "Your registration completed");
+        
+        homePage = (HomePageObject) registerPage.clickOnHeaderLink("Log out");
+    }
+
+    @Description("User_02_Login")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test
+    public void User_02_Login() {
+        loginPage = (LoginPageObject) homePage.clickOnHeaderLink("Log in");
+
+        homePage = loginPage.loginToSystem(emailAddress, password);
+
+        verifyTrue(homePage.isMyAccountLinkDisplayed());
+    }
+
+    @Description("User_03_MyAccount")
+    @Severity(SeverityLevel.MINOR)
+    @Test
+    public void User_03_MyAccount() {
+        customerInfoPage = (CustomerInfoPageObject) homePage.clickOnHeaderLink("My account");
+
+        verifyTrue(customerInfoPage.isGenderMaleSelected());
+        verifyEquals(customerInfoPage.getValueInFirstnameTextbox(), firstName);
+        verifyEquals(customerInfoPage.getValueInLastnameTextbox(), "Testing");
+//        verifyEquals(customerInfoPage.getValueInDayDropdown(), dayOfBirth);
+//        verifyEquals(customerInfoPage.getValueInMonthDropdown(), monthOfBirth);
+//        verifyEquals(customerInfoPage.getValueInYearDropdown(), yearOfBirth);
+        verifyEquals(customerInfoPage.getValueInCompanyTextbox(), companyName);
+    }
+
+}
